fe6842fd70faaa970ed9a6b51ee99fe9053fae71
Vaishnav Sanjiv Patil <vaishnav@Vaishnavs-MacBook-Air.local>
2025-04-08 14:59:48 +0530

    integration

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,14 +5,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='flex justify-between p-5 shadow-sm'>
-      <Link href={'/'} className="flex gap-2 items-center">
-        <Image alt="Logo here"src={'/logo.svg'} width={40} height={20}/>
-        <h2 className="font-bold text-2xl">LearnX AI</h2>
-        </Link>
-        <Link href={'/dashboard'}>
-          <Button className='text-white'>Get Started</Button>
-        </Link>
+    <div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Hero from "./_components/Hero";
 export default function Home() {
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
       <Hero/>
     </div>
 
